b0f07b2c9cb41b8cc29a4efa817ece55538cee73
Harsha <hvgazula@users.noreply.github.com>
2026-02-04 21:52:53 +0000

    modify first example to work with 3.12, also change cartpole version

--- spinup/examples/pytorch/pg_math/1_simple_pg.py
+++ spinup/examples/pytorch/pg_math/1_simple_pg.py
@@ -4,7 +4,7 @@ from torch.distributions.categorical import Categorical
 from torch.optim import Adam
 import numpy as np
 import gymnasium as gym
-from gym.spaces import Discrete, Box
+from gymnasium.spaces import Discrete, Box
 
 def mlp(sizes, activation=nn.Tanh, output_activation=nn.Identity):
     # Build a feedforward neural network.
@@ -57,7 +57,7 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
         batch_lens = []         # for measuring episode lengths
 
         # reset episode-specific variables
-        obs = env.reset()       # first obs comes from starting distribution
+        obs, _ = env.reset()       # first obs comes from starting distribution
         done = False            # signal from environment that episode is over
         ep_rews = []            # list for rewards accrued throughout ep
 
@@ -76,7 +76,8 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
 
             # act in the environment
             act = get_action(torch.as_tensor(obs, dtype=torch.float32))
-            obs, rew, done, _ = env.step(act)
+            obs, rew, terminated, truncated, _ = env.step(act)
+            done = terminated or truncated
 
             # save action, reward
             batch_acts.append(act)
@@ -92,7 +93,8 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
                 batch_weights += [ep_ret] * ep_len
 
                 # reset episode-specific variables
-                obs, done, ep_rews = env.reset(), False, []
+                obs, _ = env.reset()
+                done, ep_rews = False, []
 
                 # won't render again this epoch
                 finished_rendering_this_epoch = True
@@ -120,7 +122,7 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env_name', '--env', type=str, default='CartPole-v0')
+    parser.add_argument('--env_name', '--env', type=str, default='CartPole-v1')
     parser.add_argument('--render', action='store_true')
     parser.add_argument('--lr', type=float, default=1e-2)
     args = parser.parse_args()
